a3457db4942adfd1a15d1717d02cde4b62d9c26b
Amarjeet-97 <amarjeetkumardmg8409@gmail.com>
2022-11-03 00:03:47 +0530

    Attempt2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import MovieList from './MovieList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <MovieList/>
     </div>
+      
   );
 }
 
